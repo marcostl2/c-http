e4d2696d801df7ed861b1cefa3edcc0abe16721d
marcostl2 <marcossilvatl2@gmail.com>
2021-06-02 22:57:28 +0000

    kask

--- minhaVersao.c
+++ minhaVersao.c
@@ -16,23 +16,20 @@ char *replaceWord(const char *s, const char *oldW, const char *newW) {
   int newWlen = strlen(newW);
   int oldWlen = strlen(oldW);
 
-  // Counting the number of times old word
-  // occur in the string
+  // conta quantas vezes a palavra aparece na palavra antiga
   for (i = 0; s[i] != '\0'; i++) {
     if (strstr(&s[i], oldW) == &s[i]) {
       cnt++;
 
-      // Jumping to index after the old word.
       i += oldWlen - 1;
     }
   }
 
-  // Making new string of enough length
+  // novo tamanho para a string nova
   result = (char *)malloc(i + cnt * (newWlen - oldWlen) + 1);
 
   i = 0;
   while (*s) {
-    // compare the substring with the result
     if (strstr(s, oldW) == s) {
       strcpy(&result[i], newW);
       i += newWlen;
@@ -49,10 +46,7 @@ int main(int argc, char *argv[]) {
   struct addrinfo hints, *res;
   int sockfd;
   char buffer[3000];
-  char *host;
-  char *url;
-  char *n;
-  char *last;
+  char *host, *url, *n, *last;
   FILE *file;
 
   host = argv[1];
@@ -60,8 +54,6 @@ int main(int argc, char *argv[]) {
   if (strstr(host, "http://")) {
     host = replaceWord(host, "http://", "");
   }
-
-  printf("\nLast: %s\n", last);
 
   if (strstr(host, ".html")) {
     host = replaceWord(host, "/.html", "");
@@ -71,7 +63,6 @@ int main(int argc, char *argv[]) {
   last =
       strchr(host, '/'); // nesse caso pega a url restante apos a primeira barra
   url = last;
-  printf("\n%s\n", url);
 
   if (strlen(url) != 1) {
 
@@ -79,41 +70,34 @@ int main(int argc, char *argv[]) {
   }
   host = replaceWord(host, last, "");
 
-  printf("\n%s\n", url);
 
-  memset(&hints, 0, sizeof hints); // make sure the struct is empty
-  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+  memset(&hints, 0, sizeof hints); // zera a struct
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
 
   getaddrinfo(host, "80", &hints, &res);
 
+  // realiza a criação do socket
   sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  // connect!
 
+  // faz a cone
   connect(sockfd, res->ai_addr, res->ai_addrlen);
 
   char request[3000];
-  // char request[3000]="HEAD / HTTP/1.0\r\n\r\n";
+
   sprintf(request, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", url, host);
-  printf("\n%s", request);
 
   send(sockfd, request, strlen(request), 0);
   int recv_length = 1;
 
   file = fopen("file.html", "w");
-  
-  while (recv_length > 0){
-    recv_length = recv(sockfd, &buffer, 3000, 0);
-    printf("%s", buffer);
-    //fputs(buffer, file);
-    freeaddrinfo(res);
-    return 0;
-  } ;
 
-      // printf("The web server is %s\n", buffer + 8);
-
-      printf("\nsem drama\n");
+  while (recv_length > 0) {
+    recv_length = recv(sockfd, &buffer, sizeof(buffer), 0);
+    fputs(buffer + 8, file);
+  };
+  freeaddrinfo(res);
   fclose(file);
 
   return 0;
---
+++ zuera.c
@@ -0,0 +1,49 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+int main(void) {
+  struct addrinfo hints, *results;
+  // struct sockaddr_in *ip_access_temp;
+  int rv;
+  int sockfd;
+  char inputVal[100];
+  unsigned char buffer[4096];
+  printf("Insira o dominio do site: \n");
+  scanf("%s", inputVal);
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo(inputVal, "80", &hints, &results)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  sockfd =
+      socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+
+  connect(sockfd, results->ai_addr, results->ai_addrlen);
+
+  send(sockfd, "HEAD Accept: text/html / HTTP/1.0\r\n\r\n", 23, 0);
+  int recv_length = 1;
+  recv_length = recv(sockfd, &buffer, 1024, 0);
+  while (recv_length > 0) {
+    printf("The web server is %s\n", buffer + 8);
+    freeaddrinfo(results);
+    return 0;
+  }
+
+  printf("Server line not found\n");
+
+  freeaddrinfo(results);
+
+  printf("\n");
+
+  return 1;
+}
